49e2b68cc314a41490c61b5c6aa2247eba3082db
kushagrasharma2499 <105211353+kushagrasharma2499@users.noreply.github.com>
2022-11-24 22:49:29 +0530

    Update app.js

--- question 1/app.js
+++ question 1/app.js
@@ -1,1 +1,21 @@
+const cafes = require("./cafe");
+const places = require("./place");
 
+const findTheCafes = (cafe) => {
+  const getCafes = cafes.filter((item) =>
+    item.name.toLowerCase().includes(cafe.trim().toLowerCase())
+  );
+  const matchThePlaces = [];
+  for (let cafe of getCafes) {
+    const getPlace = places.find((place) => cafe.place_id === place.id);
+    delete cafe.place_id;
+    delete getPlace.id;
+
+    matchThePlaces.push({ ...cafe, ...getPlace });
+  }
+  return matchThePlaces;
+};
+
+const getRequiredPlace = findTheCafes("Philz");
+
+console.log(getRequiredPlace);
